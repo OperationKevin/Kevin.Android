c373207636f0d6e7773dfcd95f87a327b4834887
Mike Tamis <tamis.mike@gmail.com>
2014-05-25 16:51:37 +1200

    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Kevin.Core;
+
+namespace Kevin.Android
+{
+	[Activity (Label = "Kevin for Android", MainLauncher = true)]
+	public class MainActivity : Activity, Output
+	{
+		KevinCore core;
+
+		TextView output;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			core = new KevinCore (this);
+			// Get our button from the layout resource,
+			// and attach an event to it
+
+			EditText input = FindViewById<EditText> (Resource.Id.input);
+			output = FindViewById<TextView> (Resource.Id.textView1);
+			input.KeyPress += (object sender, View.KeyEventArgs e) => {
+				e.Handled = false;
+				if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter) {
+					e.Handled = true;
+					output.Append ("you: " + input.Text + "\n");
+					core.tell(input.Text);
+					input.Text = "";
+				}
+			};
+		}
+
+		#region Output implementation
+
+		public void send (string hey)
+		{
+			output.Append ("Kevin: " + hey + "\n");
+		}
+
+		#endregion
+	}
+}
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Kevin.Desktop
+==========
+This is a Android interface to Kevin
+
+![alt tag](https://raw.githubusercontent.com/OperationKevin/Kevin.Android/master/show.png)
